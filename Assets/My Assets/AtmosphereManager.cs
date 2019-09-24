0623be1762415bd015abc2f38f65661f7648e83f
Michelle Anne Roxas <michanroxas@gmail.com>
2019-09-24 14:22:44 -0400

    Added 2 songs, Person Material, Lighting Settings

--- Assets/My Assets/AtmosphereManager.cs
+++ Assets/My Assets/AtmosphereManager.cs
@@ -9,13 +9,17 @@ public class AtmosphereManager : MonoBehaviour
 
     Material skybox;
     //public Quaternion sunRotation;
-  //  public bool sunOn; //Is the sun active or not?
-   // public Transform sun;
-    AudioClip music;
+    //  public bool sunOn; //Is the sun active or not?
+    // public Transform sun;
+    public AudioClip music;
+    AudioSource audioSource;
 
 
     void Awake()
     {
+        //Get audio source
+        audioSource = GetComponent<AudioSource>();
+
         //Generate random number and pick properties based on it
         if (!gameManager.Instance.debugMode)
             num = Random.Range(1, 5);
@@ -24,35 +28,54 @@ public class AtmosphereManager : MonoBehaviour
         {
             case 1: //Daybreak
                 skybox = Resources.Load<Material>("CloudyCrown_Daybreak");
-                               // sun.gameObject.SetActive(false);
+                // sun.gameObject.SetActive(false);
 
                 break;
             case 2: //Midday
                 skybox = Resources.Load<Material>("CloudyCrown_Midday");
+                music = Resources.Load<AudioClip>("After_All");
+                audioSource.volume = 0.06f;
+                audioSource.pitch = 1;
                 break;
             case 3: //Sunset
                 skybox = Resources.Load<Material>("CloudyCrown_Sunset");
-               // sun.gameObject.SetActive(false);
+                // sun.gameObject.SetActive(false);
+                //Affect light source too
+                music = Resources.Load<AudioClip>("After_All");
+                audioSource.volume = 0.186f;
+                audioSource.pitch = 0.5f;
                 break;
             case 4: //Evening
                 skybox = Resources.Load<Material>("CloudyCrown_Evening");
-               // sun.gameObject.SetActive(false);
+                // sun.gameObject.SetActive(false);
                 break;
             case 5: //Midnight
                 skybox = Resources.Load<Material>("CloudyCrown_Midnight");
-               // sun.gameObject.SetActive(false);
+                // sun.gameObject.SetActive(false);
+                music = Resources.Load<AudioClip>("Eddy");
+                audioSource.volume = 0.186f;
+                audioSource.pitch = 1;
                 break;
         }
-
     }
 
     // Start is called before the first frame update
     void Start()
     {
-
         //Change skybox material
         RenderSettings.skybox = skybox;
         DynamicGI.UpdateEnvironment();
+
+        //Assign music and play
+        audioSource.clip = music;
+        if (audioSource.clip)
+        {
+            audioSource.Play();
+        }
+        else
+        {
+            Debug.Log("No song assigned to AtmosphereManager...");
+        }
 
     }
 
